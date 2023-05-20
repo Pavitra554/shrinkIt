9be82acea3b37b7a585752418b147462e1a6c01a
PAVITRA BEHARA <pavitraredmi@gmail.com>
2023-05-20 23:53:33 +0530

    ADD:cors added

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 require('./db/mongoose')
 const main = require('./routes/main')
+const cors = require('cors')
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()) 
 app.use(express.json())
 app.use(main)
 
